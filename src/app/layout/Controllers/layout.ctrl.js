2a84cecd3a5ebd665aae1d07b114d4a928e8d938
KING DEV <kd19850511@gmail.com>
2017-02-23 11:44:28 -0800

    added window.prerender ready flag

--- src/app/layout/Controllers/layout.ctrl.js
+++ src/app/layout/Controllers/layout.ctrl.js
@@ -203,6 +203,8 @@
             if ($rootScope.DEBUG_MODE) console.log("@loadingDone - $rootScope.dataIsLoaded -", $rootScope.dataIsLoaded);
             if ($rootScope.DEBUG_MODE) console.log("@loadingDone - $rootScope.pageDataLoaded -", $rootScope.pageDataLoaded);
             if ($rootScope.DEBUG_MODE) console.log("@loadingDone - $rootScope.userDataLoaded -", $rootScope.userDataLoaded);
+
+            $window.prerenderReady = true;
             /*
             console.log("Stored data in cookies!")
 
