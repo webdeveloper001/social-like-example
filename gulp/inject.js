4817cbf28dca3d093daf4784b29cbd04c8429def
KING DEV <kd19850511@gmail.com>
2017-03-03 21:38:26 -0800

    moved mybiz css and font content into the my business page to not overwrite other pages css

--- gulp/inject.js
+++ gulp/inject.js
@@ -11,7 +11,9 @@ var _ = require('lodash');
 
 gulp.task('inject', ['scripts'], function () {
   var injectStyles = gulp.src([
-    path.join(conf.paths.src, '/app/**/*.css')
+    path.join(conf.paths.src, '/app/**/*.css'),
+    path.join(conf.paths.src, '/assets/css/*.css'),
+    path.join(conf.paths.src, '/assets/css/**/*.css')
   ], { read: false });
 
   var injectScripts = gulp.src([
