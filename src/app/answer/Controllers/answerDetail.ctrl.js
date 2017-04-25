9b9fd6d748b77a7cb9e7fc6c58222cf0326b4658
KING DEV <kd19850511@gmail.com>
2017-04-25 00:13:27 -0700

    Changed rankSummary layout to tiles Changes answerDetail layout to tiles with map Modified cwrapper loadContent to select top3 only when images are good

--- src/app/answer/Controllers/answerDetail.ctrl.js
+++ src/app/answer/Controllers/answerDetail.ctrl.js
@@ -89,6 +89,20 @@
             || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4)))
             vm.isMobile = true;
     
+        //Effective width vs $window.innerWidth
+        var iWa = [0,   769, 971, 996, 1173,1201,1635,3500];
+        var eWa = [0.96,0.96,0.75,0.96,0.68,0.96,0.64,0.6];
+        var iW = $window.innerWidth;
+        var eW = 0; //effective width
+        for (var n=0; n < iWa.length-1; n++){
+            if (iW > iWa[n] && iW < iWa[n+1] ){
+                //equation of line y=mx+b (m is slope, b is y intersect)
+                var slp = (eWa[n+1]-eWa[n])/(iWa[n+1]-iWa[n]);
+                var bpt = eWa[n] - iWa[n]*slp;
+                eW = iW * slp + bpt;
+            }
+        }
+
         //Adjust picture size for very small displays
         if ($window.innerWidth < 512) {
             vm.mxheight = '250';
@@ -96,7 +110,7 @@
             vm.sp2 = 'width:25%;max-height:50px';
             vm.sp3 = 'width:20%';
             vm.sm = true; vm.nsm = false;
-            vm.width = Math.round($window.innerWidth * 0.9);
+            vm.width = Math.round(iW*eW);
             //console.log('screen is small');
         }
         else {
@@ -105,8 +119,9 @@
             vm.sp2 = 'width:22.5%;max-height:50px;';
             vm.sp3 = 'width:20%';
             vm.sm = false; vm.nsm = true;
-            vm.width = Math.round($window.innerWidth * 0.9);
-            //console.log('screen is big');
+            vm.width = Math.round(iW*eW);
+            vm.width2 = Math.round((iW*eW)/2);
+            vm.width3 = Math.round((iW*eW)/3);
         }
         
         //TODO: Would like to add this abstract template, but dont know how               
@@ -164,6 +179,8 @@
 
             $rootScope.canswer = vm.answer;
             vm.type = vm.answer.type;
+            vm.isShortPhrase = vm.type == 'Short-Phrase';
+            vm.hasMap = vm.answer.location != undefined && vm.answer.location != ''; 
 
             //if there is no category, look for it in cookies
             if ($rootScope.cCategory == undefined) {
@@ -773,10 +790,13 @@
                             rankObj.upVi = $rootScope.catansrecs[i].upV;
                             rankObj.downVi = $rootScope.catansrecs[i].downV;
 
-                            if (rankObj.rank == 1) rankObj.icon = "/assets/images/first.png";
-                            else if (rankObj.rank == 2) rankObj.icon = "/assets/images/second.png";
-                            else if (rankObj.rank == 3) rankObj.icon = "/assets/images/third.png";
-                            else if (rankObj.rank > 3 && rankObj.rank < 11) rankObj.icon = "/assets/images/topten.png";
+                            if (rankObj.rank == 1) rankObj.icon = "/assets/images/gold_.png";
+                            else if (rankObj.rank == 2) rankObj.icon = "/assets/images/silver_.png";
+                            else if (rankObj.rank == 3) rankObj.icon = "/assets/images/bronze_.png";
+                            else if (rankObj.rank > 3 && rankObj.rank < 11) rankObj.icon = "/assets/images/top_10.png";
+                            else if (rankObj.rank >= 11 && rankObj.rank < 21) rankObj.icon = "/assets/images/top_20.png";
+                            else if (rankObj.rank >= 21 && rankObj.rank < 51) rankObj.icon = "/assets/images/top_50.png";
+                            else if (rankObj.rank >= 51 && rankObj.rank < 101) rankObj.icon = "/assets/images/top_100.png";
                             else rankObj.icon = "/assets/images/blank.png";
    
                             //TODO insert rank position out of total list, will be in catans
