18fe75fa6e1424497bdeebebda25225500ea770d
KING DEV <kd19850511@gmail.com>
2017-05-25 00:32:46 +0800

    Change facebook app id

--- src/app/config.js
+++ src/app/config.js
@@ -12,7 +12,7 @@
         // My App ID: 1494723870571848
         // Rank-X: 1102409523140826
         $httpProvider.interceptors.push('httpInterceptor');
-        $facebookProvider.setAppId('1494723870571848');
+        $facebookProvider.setAppId('1102409523140826');
         
         //SCE 
         $sceDelegateProvider.resourceUrlWhitelist([
