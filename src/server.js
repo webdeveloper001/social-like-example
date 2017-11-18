cd86d51bae348e969af2d0de32b0f634a46b6a4d
KING DEV <kd19850511@gmail.com>
2017-11-18 15:17:17 -0500

    add mangle option for uglify

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = module.exports = express();
 
-app.configure(function(){ 
+// app.configure(function(){ 
   // Here we require the prerender middleware that will handle requests from Search Engine crawlers 
   // We set the token only if we're using the Prerender.io service 
   app.use(require('prerender-node').set('prerenderToken', 'RUfTYPRmORizHiodvGJF'));
-  app.use(express.static("public")); app.use(app.router); 
-});
+  app.use(express.static("public")); 
+  // app.use(app.router); 
+// });
 
 // This will ensure that all routing is handed over to AngularJS 
 app.get('*', function(req, res){ 
